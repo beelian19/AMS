f7b2c5cff1250d23d1f80df713c50a8cb7827de6
YeMinOo <minoo.ye.2015@sis.smu.edu.sg>
2018-01-02 16:06:03 +0800

    Added UpdateProjectHours

--- src/main/java/Module/ProjectManagement/UpdateProjectHours.java
+++ src/main/java/Module/ProjectManagement/UpdateProjectHours.java
@@ -5,9 +5,10 @@
  */
 package Module.ProjectManagement;
 
-/**
- *
- * @author yemin
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 import DAO.EmployeeDAO;
@@ -15,9 +16,6 @@ import DAO.ProjectDAO;
 import Entity.Employee;
 import Entity.Project;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashSet;
-import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +25,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Lin
  */
-public class UpdateProjectCompletionStatus extends HttpServlet {
+public class UpdateProjectHours extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -40,45 +38,42 @@ public class UpdateProjectCompletionStatus extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //System.out.println("TEST" + request.getParameter("projectIden"));
         Employee e = EmployeeDAO.getEmployeeByID((String) request.getSession().getAttribute("userId"));
         String name = e.getName();
-        Boolean isAssignedEmp = false;
-        Boolean isAssignedRev = false;
-        if (e.getIsAdmin().equals("yes")) {
-            isAssignedEmp = true;
-            isAssignedRev = true;
-        }
         String pID;
         Project p;
+        Double emp1;
+        Double emp2;
         if (request.getParameter("projectIden") != null) {
             pID = (String) request.getParameter("projectIden");
             p = ProjectDAO.getProjectByID(pID);
-
-            if (p.getEmployee1().equals(name) || p.getEmployee2().equals(name)) {
-                isAssignedEmp = true;
-            }
-            if (p.getProjectReviewer().equals(name)) {
-                isAssignedRev = true;
-            }
-
-            if (request.getParameter("review") != null && isAssignedRev
-                    && p.getProjectReviewStatus().equals("incomplete")) {
-                p.setProjectReviewStatus("complete");
-            } else if (request.getParameter("complete") != null && isAssignedEmp
-                    && p.getProjectStatus().equals("incomplete")) {
-                p.setProjectStatus("complete");
+            if (request.getParameter("employee1Hours").equals("")) {
+                emp1 = 0.0;
             } else {
-                request.getSession().setAttribute("status", "Error: No change to project statuses");
-                String url = "ProjectProfile.jsp?projectID=" + p.getProjectID();
-                response.sendRedirect(url);
+                emp1 = Double.parseDouble(request.getParameter("employee1Hours"));
+            }
+            if (request.getParameter("employee2Hours") == null || request.getParameter("employee2Hours").equals("") ) {
+                emp2 = 0.0;
+            } else {
+                emp2 = Double.parseDouble(request.getParameter("employee2Hours"));
+            }
+            
+            if (p.getEmployee1().equals(name)) {
+                p.setEmployee1Hours(emp1);
+            } else if (p.getEmployee2().equals(name)) {
+                p.setEmployee2Hours(emp2);
+            } else if (e.getIsAdmin().equals("yes")) {
+                p.setEmployee1Hours(emp1);
+                p.setEmployee2Hours(emp2);
             }
 
             if (ProjectDAO.updateProject(p)) {
-                request.getSession().setAttribute("status", "Success: Status successfully updated!");
+                request.getSession().setAttribute("status", "Success: Hours successfully updated!");
                 String url = "ProjectProfile.jsp?projectID=" + p.getProjectID();
                 response.sendRedirect(url);
             } else {
-                request.getSession().setAttribute("status", "Error: Status failed to update!");
+                request.getSession().setAttribute("status", "Error: Hours failed to update!");
                 String url = "ProjectProfile.jsp?projectID=" + p.getProjectID();
                 response.sendRedirect(url);
             }
